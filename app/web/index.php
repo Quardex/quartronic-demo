a0bb789fcb506befae48b11fced672818024ce93
Andrew Quardex <megasounds@mail.ru>
2020-02-04 20:40:42 +0300

    no message

--- app/web/index.php
+++ app/web/index.php
@@ -3,6 +3,6 @@
 require_once '../../vendor/autoload.php';
 
 $QApp = new \quarsintex\quartronic\qcore\Quartronic;
-$QApp->run(['appDir' => '../quartronic-demo/app']);
+$QApp->run(['appDir' => '../../../app']);
 
 ?>
