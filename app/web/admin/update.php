4db75a28ff00a70a163f398cc4a4bba957f6b54a
Andrew Quardex <megasounds@mail.ru>
2020-04-15 02:12:50 +0300

    no message

--- app/web/admin/update.php
+++ app/web/admin/update.php
@@ -1,9 +1,7 @@
 <?
 require_once '../../../vendor/autoload.php';
 
-echo "\n";
 echo "Start updating...\n";
-file_put_contents(__DIR__.'/update.lock', 1);
 \quarsintex\quartronic\qcore\QUpdater::run(__DIR__.'/../../../');
 
 ?>
